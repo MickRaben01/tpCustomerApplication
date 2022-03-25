8442e3285ead71227503b76c2cf7a69215c78158
mickaelrabenanahary <raben.mickael@gmail.com>
2022-03-25 15:23:58 +0300

    Etat initial

---
+++ src/main/java/com/micka/tpcustomerapplication/JakartaRestConfiguration.java
@@ -0,0 +1,15 @@
+package com.micka.tpcustomerapplication;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+
+/*Test commit*/
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
