fd1f54b1bf80a20020c668fedd41483339ef9455
mickaelrabenanahary <raben.mickael@gmail.com>
2022-03-27 00:10:43 +0300

    Ajout DataTable JSF dans la page

---
+++ src/main/java/ConfigJSF.java
@@ -0,0 +1,18 @@
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author mickaelrabenanahary
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
